5a9f8a165ecbe0fb82700001d389056c4f42aedd
RobijnDM <henriques.gabriel@outlook.com>
2018-12-02 04:29:10 -0200

    Adding Routes to Users

--- server.js
+++ server.js
@@ -32,6 +32,23 @@ app.post('/register', async (req,res) => {
     
 })
 
+app.get('/users', async(req,res) => {
+    User.findAll().then(users => {res.json(users)}).catch(err => res.json(err));
+})
+
+app.get('/user/:id', async (req,res) => {
+    User.findOne({where:{id: req.params.id}})
+    .then(user => res.json(user))
+    .catch(err => res.json(err))
+})
+
+app.put('/user/:id', async (req,res) => {
+    User.update({name: req.body.name, email:req.body.email, password: req.body.password},
+        {returning: true, where: {id: req.params.id}})
+        .then(([rowsUpdated, [updatedUser]]) => res.json(updatedUser))
+        .catch(err => res.json(err))
+});
+
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);
 })
